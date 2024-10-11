464e52b006bd1a6d9ce35811ad7e753c409321c2
Mona Lisa <wahbanous@gmail.com>
2024-10-10 20:49:49 -0400

    RemoveCity function

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -17,6 +17,7 @@ router.post('/', async (req: Request, res: Response) => {
     const data = await weatherService.getData(coordinates[0].lat, coordinates[0].lon);
 
     historyService.addCity(cityName);
+
     //respond with data - NOTE: WE NEED TO TRANSFORM SHAPE THE DATA INTO WHAT THE FROM-END IS EXPECTING - this res.json(data) might need to be done below - using historyService to store current city weather info - refer to week9 mini project for example.
     //res.json(data)
     res.json(data);
@@ -30,6 +31,18 @@ router.get('/history', async (_req: Request, res: Response) => {
   const cities = await historyService.getCities();
   res.json(cities);
 });
+
 // * BONUS TODO: DELETE city from search history
-//router.delete('/history/:id', async (req: Request, res: Response) => {});
+router.delete('/history/:id', async (req: Request, res: Response) => {
+  try {
+    if (!req.params.id) {
+      res.status(400).json({ msg: 'State id is required' });
+    }
+    await historyService.removeCity(req.params.id);
+    res.json({ success: 'State successfully removed from search history' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 export default router;
